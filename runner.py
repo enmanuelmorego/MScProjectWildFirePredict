3183bb99d5b2c1514ec88896e3438782a269df49
enmanuelmorego <143163660+enmanuelmorego@users.noreply.github.com>
2026-01-25 18:13:20 +0000

    TEsting data directory

--- runner.py
+++ runner.py
@@ -9,6 +9,9 @@ import src.config as c
 print("🔎 PROJECT_ROOT:")
 print(f"\t{c.PROJECT_ROOT}")
 
+print("🔎 DATA DIRECTORY:")
+print(f"\t{os.environ.get('DATA_DIR')}")
+
 import src.load_data as ld
 viirs_files = ld.get_filepaths('VIIRS')
 viirs_to_load = ld.to_load_viirs(viirs_files,[2018])
