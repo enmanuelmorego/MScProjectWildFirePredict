3c2bc3483f582fe565ee6890bd3c4ba2a75fc3eb
enmanuelmorego <143163660+enmanuelmorego@users.noreply.github.com>
2026-01-25 17:54:24 +0000

    Testing a runner

---
+++ runner.py
@@ -0,0 +1,16 @@
+import os
+
+# MUST be first
+os.environ.setdefault("RUN_DEMO", "OFF")
+
+print("🏁 Running the program has started!! ")
+
+import src.config as c
+print("🔎 PROJECT_ROOT:")
+print(f"\t{c.PROJECT_ROOT}")
+
+import src.load_data as ld
+viirs_files = ld.get_filepaths('VIIRS')
+viirs_to_load = ld.to_load_viirs(viirs_files,[2018])
+print(f"🧪 Testing file selection: ")
+print(f"\t{viirs_to_load}")
