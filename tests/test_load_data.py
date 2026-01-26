3e514b88d7f26ec2cec6345f1e1708d016742931
enmanuelmorego <143163660+enmanuelmorego@users.noreply.github.com>
2026-01-26 15:08:29 +0000

    Created unit tests for merge_viirs()

--- tests/test_load_data.py
+++ tests/test_load_data.py
@@ -25,7 +25,43 @@ def test_merge_viirs_merges_df_correctly():
     
     test_viirs_data = {'snpp': pd.DataFrame(d1),
                        'noaa': pd.DataFrame(d2)}
-    df_test = ld.merge_viirs(test_viirs_data)
+    dict_test = ld.merge_viirs(test_viirs_data)
     df_expect = pd.DataFrame(de)
-    assert_frame_equal(df_expect, df_test.get('df'))
+    assert_frame_equal(df_expect, dict_test.get('df'))
+
+def test_merge_viirs_ignores_noaa():
+    d1 = {'longitude':   [1,            2,             3,           4,            5],
+          'latitude':    [10,           11             ,12,         13,           14],
+          'acq_date':    ['2024-01-01', '2024-02-02', '2024-03-03', '2024-04-04', '2024-05-05'],
+          'another':     [0.1,          0.2,           0.3,         0.4,           0.5]}
+    d2 = {'longitude':   [1,            3,            9,            4,            5],
+          'latitude':    [10,           11,           12,           20,           14],
+          'acq_date':    ['2024-01-01', '2024-02-02', '2025-03-03', '2024-04-04', '2024-05-05'],
+          'another':     [0.1,          0.2,           0.3,          0.4,          0.101]}
     
+    test_viirs_data = {'snpp': pd.DataFrame(d1),
+                       'noaa': pd.DataFrame(d2)}
+    dict_test = ld.merge_viirs(test_viirs_data, False)
+    assert_frame_equal(test_viirs_data.get('snpp'), dict_test.get('df'))
+    
+def test_merge_viirs_data_report():
+    d1 = {'longitude':   [1,            2,             3,           4,            5],
+          'latitude':    [10,           11             ,12,         13,           14],
+          'acq_date':    ['2024-01-01', '2024-02-02', '2024-03-03', '2024-04-04', '2024-05-05'],
+          'another':     [0.1,          0.2,           0.3,         0.4,           0.5]}
+    d2 = {'longitude':   [1,            3,            9,            4,            5],
+          'latitude':    [10,           11,           12,           20,           14],
+          'acq_date':    ['2024-01-01', '2024-02-02', '2025-03-03', '2024-04-04', '2024-05-05'],
+          'another':     [0.1,          0.2,           0.3,          0.4,          0.101]}
+    de = {'longitude':   [1,            2,             3,           4,            5,             3,           9,            4          ],
+          'latitude':    [10,           11             ,12,         13,           14,            11,          12,           20         ],
+          'acq_date':    ['2024-01-01', '2024-02-02', '2024-03-03', '2024-04-04', '2024-05-05',  '2024-02-02','2025-03-03', '2024-04-04'],
+          'another':     [0.1,          0.2,           0.3,         0.4,           0.5,          0.2,          0.3,          0.4       ]}
+    
+    test_viirs_data = {'snpp': pd.DataFrame(d1),
+                       'noaa': pd.DataFrame(d2)}
+    dict_test = ld.merge_viirs(test_viirs_data)
+    dict_data_report = dict_test.get('data_report')
+    assert dict_data_report.get('total_rows_snpp') == 5
+    assert dict_data_report.get('total_rows_noaa') == 3
+     
