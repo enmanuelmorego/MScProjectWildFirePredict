2e1bcff5876a084423cb4537b2c5037f1b9a60dd
enmanuelmorego <143163660+enmanuelmorego@users.noreply.github.com>
2026-01-29 07:57:41 +0000

    Developing how to load sentinel images/data

--- runner.py
+++ runner.py
@@ -44,4 +44,5 @@ print(f"UK Grid Daily")
 dates = u.extract_year_range(df_viirs)
 df_daily_grid = df_uk_grid.copy()
 df_daily_grid['join_key'] = 1
-df_daily_grid = df_daily_grid.merge(dates, on='join_key').drop(columns='joi
+df_daily_grid = df_daily_grid.merge(dates, on='join_key').drop(columns='join_key')
+print(f"Shape: {df_daily_grid.shape}")
