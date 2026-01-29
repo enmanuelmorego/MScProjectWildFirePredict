ac00548f7ed6a51c646ce92046d3738e2c50db96
enmanuelmorego <143163660+enmanuelmorego@users.noreply.github.com>
2026-01-29 06:15:22 +0000

    Developing Sentinel-2 image downloader

--- runner.py
+++ runner.py
@@ -44,5 +44,4 @@ print(f"UK Grid Daily")
 dates = u.extract_year_range(df_viirs)
 df_daily_grid = df_uk_grid.copy()
 df_daily_grid['join_key'] = 1
-df_daily_grid = df_daily_grid.merge(dates, on='join_key').drop(columns='join_key')
-print(f"Shape: {df_daily_grid.shape}")
+df_daily_grid = df_daily_grid.merge(dates, on='join_key').drop(columns='joi
