4d0b1dcde901c63e09ecbbee2faa89d7ab437985
enmanuelmorego <143163660+enmanuelmorego@users.noreply.github.com>
2026-02-23 21:37:50 +0000

    Updated regex pattern in save_plots

--- src/utils.py
+++ src/utils.py
@@ -129,7 +129,7 @@ def save_plots(plot_obj: Figure, plot_title: str, run_id: str) -> None:
   """
   path = f"outputs/{run_id}"
   os.makedirs(path, exist_ok = True)
-  plot_title = re.sub("\\[a-z]","", plot_title)
+  plot_title = re.sub(r"[\n\t]","", plot_title)
   fout = f"{path}/{run_id}_{plot_title}.pdf"
   try:
     plot_obj.savefig(fout, bbox_inches = "tight")
