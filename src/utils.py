2bafc4a96eaf4ad12bbf4e3140b82a7e76f818f4
enmanuelmorego <143163660+enmanuelmorego@users.noreply.github.com>
2026-03-07 08:47:05 +0000

    Tested split_df_by_year function

--- src/utils.py
+++ src/utils.py
@@ -157,14 +157,3 @@ def split_df_by_year(df: pd.DataFrame) -> dict:
     df_subset   = df_subset.drop(columns = ['year'])
     out_dict[y] = df_subset
   return out_dict
-
-if __name__ == "__main__":
-  df_test = pd.DataFrame({'date': pd.to_datetime(['2025-01-01', '2026-01-01', '2027-01-01', '2027-01-01'])})
-  df = split_df_by_year(df_test)
-  df_test     = pd.DataFrame({'date': pd.to_datetime(['2025-01-01', '2026-01-01', '2027-01-01', '2027-01-01']),
-                              'name': ['A','B','C','D']})
-  dict_test   = split_df_by_year(df_test)
-  cols_expect = ['date', 'name']
-
-  for _, v in dict_test.items():
-    print(f"{list(v.columns)} == {cols_expect}")
