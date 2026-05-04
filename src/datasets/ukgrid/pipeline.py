cf483303a1f7a6c6b2830e5eaff216c29d4c544b
enmanuelmorego <143163660+enmanuelmorego@users.noreply.github.com>
2026-05-04 14:47:53 +0100

    refactored uk grid functions

--- src/datasets/ukgrid/pipeline.py
+++ src/datasets/ukgrid/pipeline.py
@@ -1,16 +1,13 @@
 import src.datasets.ukgrid.loader as l
-
 from pathlib import Path
+import pandas as pd
+import geopandas as gpd
 
-def load_ukgrid_main(data_dir: Path, file_name: str, crs: str):
+def load_ukgrid_main(df_days_in: pd.DataFrame, data_dir: Path, file_name: str, crs: str) -> dict[str, gpd.GeoDataFrame]:
 
-    df_ukgrid = l.load_uk_grid(data_dir, file_name, crs)
-    return df_ukgrid
-
-if __name__ == "__main__":
-    # python3 -m src.datasets.ukgrid.pipeline
-    PROJ_HOME = Path(__file__).resolve().parents[3] 
-    data_dir = Path(PROJ_HOME)/"data"
-
-    x = load_ukgrid_main(data_dir, "ukcp18-uk-land-12km.shp", "EPSG: 4326")
-    print(x.head())
+    df_ukgrid                 = l.load_uk_grid(data_dir, file_name, crs)
+    df_daily_grid             = df_ukgrid.copy()
+    df_daily_grid['join_key'] = 1
+    df_daily_grid             = df_daily_grid.merge(df_days_in, on='join_key').drop(columns='join_key')
+    return {'df_ukgrid'   : df_ukgrid,
+            'df_daily_grid': df_daily_grid}
--- src/scripts/run_tabular.py
+++ src/scripts/run_tabular.py
@@ -2,9 +2,12 @@
 Module to run all the data load, and transformations to prepare all tabular data for transformations
 """
 
-from src.scripts.set_parameters import VALIDATION_DATE, PARAMETERS
 import src.scripts.validation_checks as vc
+import src.utils.datasets_utils as du
+
+from src.scripts.set_parameters import VALIDATION_DATE, PARAMETERS
 from src.datasets.viirs.pipeline import load_viirs_main
+from src.datasets.ukgrid.pipeline import load_ukgrid_main
 
 def run_tabular():
     # Validation rule
@@ -13,18 +16,28 @@ def run_tabular():
     YEAR_FILTER = PARAMETERS['YEAR_FILTER']
     DATA_DIR    = PARAMETERS['DATA_DIR']
     CRS         = PARAMETERS['CRS'] 
+    SP_FILENAME = PARAMETERS['SP_FILENAME']
     # Load VIIRS data
     dict_viirs = load_viirs_main(years_to_load = YEAR_FILTER, data_dir = DATA_DIR, crs = CRS)
     df_viirs = dict_viirs['df_viirs']
+    # Generate dataframe with every day from min to max of df_viirs
+    df_days = du.extract_year_range(df_viirs)
+
     # Load UK Grid data
-    
+    dict_ukgrid     = load_ukgrid_main(df_days_in = df_days, data_dir = DATA_DIR, file_name = SP_FILENAME, crs = CRS)
+    df_ukgrid       = dict_ukgrid['df_ukgrid']
+    df_daily_grid   = dict_ukgrid['df_daily_grid']
+    print(df_ukgrid.keys())
+    print(type(df_ukgrid))
+    print(type(df_daily_grid))
 
 
-    return df_viirs
+
+    return None
 
 if __name__ == "__main__":
     # python3 -m src.scripts.run_tabular
     
     x = run_tabular()
-    print(x.head())
+    #print(x.head())
     
