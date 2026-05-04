78cfd2f65f33b1552d95b256684f3b264915fd2e
enmanuelmorego <143163660+enmanuelmorego@users.noreply.github.com>
2026-05-04 11:57:53 +0100

    created parameter file in scripts as the main place to udpate variables

---
+++ src/scripts/set_parameters.py
@@ -0,0 +1,27 @@
+"""
+Module to set the parameters for the run and ensure user is updating the file before running pipeline(s)
+"""
+from datetime import date, datetime
+import os
+
+os.environ.setdefault("RUN_DEMO", "ON")
+
+# Enter date as yyyy, m, d
+validation_date = date(2026, 5, 4)
+
+parameters = {"YEAR_FILTER"      : [2018],
+              "CRS"              : "EPSG: 4326",          # Set Coordinate Reference System (CRS) so it is uniform across all data inputs
+              "SATELLITE_IMAGES" : "COPERNICUS/S2_SR_HARMONIZED",
+              "SATELLITE_BANDS"  : ["B3","B4","B8"],
+              "SATELLITE_SCALE"  : 80,
+              "GRB_NAME"         : "Forest fire weather index (as defined by the Canadian Forest Service)",
+              "DATA_DIR"         : os.environ["DATA_DIR"],
+              "RUN_ID"           : f"{datetime.strftime(datetime.now(), '%Y%m%d%H%M')}_RUNNING_DEMO_{os.environ.get('RUN_DEMO')}",
+              "RANDOM_SEED"      : 42,
+              
+              "VIIRS_DIR"                : "VIIRS",
+              "FWI_DIR"                  : "FWI",
+              "FIRENOFIRE_SAMPLED_DIR"   : "SampledFireNoFire",
+              "FIRENOFIRE_SAMPLED_FNAME" : "sampled_firenofire.csv",
+              "SP_FILENAME"              :"ukcp18-uk-land-12km.shp",
+              "GEE_PROJECT"              : "ee-enmanuelmorego"}
