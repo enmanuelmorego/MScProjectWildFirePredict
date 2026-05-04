ca1bd016eaa13c2dd7754abc75efefa1d0dadc70
enmanuelmorego <143163660+enmanuelmorego@users.noreply.github.com>
2026-05-04 10:02:31 +0100

    moved get_filepaths to new utils folder

---
+++ src/utils/file_utils.py
@@ -0,0 +1,18 @@
+"""
+Module to manage and interact with files and directory
+"""
+from pathlib import Path
+import os
+
+def get_filepaths(dir_name: str) -> list[Path]: 
+    """  Function to get all the files in a directory inside the data folder
+
+    Args:
+        dir_name (str): Name of the directory inside of data folder to get all the files names from 
+
+    Returns:
+        list[Path]: List containing all the files inside the given folder
+    """  
+    dir_path = Path(os.environ['DATA_DIR'])/dir_name
+    files = list(dir_path.iterdir())
+    return files
