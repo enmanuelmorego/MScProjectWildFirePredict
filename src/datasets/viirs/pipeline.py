3aa605bc56c3e47bf4641f50164f8f3d9cba032a
enmanuelmorego <143163660+enmanuelmorego@users.noreply.github.com>
2026-05-04 11:10:46 +0100

    ADded full boilerplate for the viirs pipeline forease

--- src/datasets/viirs/pipeline.py
+++ src/datasets/viirs/pipeline.py
@@ -1,5 +1,6 @@
 import src.utils.file_utils as fu
 import src.datasets.viirs.loader as l
+import src.datasets.viirs.transforms as t
 
 # Temp
 import os
@@ -8,11 +9,21 @@ os.environ.setdefault("DATA_DIR", str(Path(__file__).resolve().parents[3] / "dat
 # temp
 
 def load_viirs_main(years_to_load: list[int]):
-    viirs_files   = fu.get_filepaths('VIIRS', "csv")
-    viirs_to_load = l.select_viirs_files(viirs_files, years_to_load)
-    viirs_data    = l.load_viirs(viirs_to_load)
+    viirs_files     = fu.get_filepaths('VIIRS', "csv")
+    viirs_to_load   = l.select_viirs_files(viirs_files, years_to_load)
+    viirs_data      = l.load_viirs(viirs_to_load)
+    df_viirs_raw    = t.merge_viirs(viirs_data)
+    df_viirs_report = df_viirs_raw['data_report']
+    df_viirs_temp   = df_viirs_raw['df']
+  
+    df_viirs         = filter_viirs(df_viirs_temp)
+    df_viirs         = df_viirs.rename(columns = {'acq_date': 'date'})
+    df_viirs["date"] = pd.to_datetime(df_viirs["date"])
+    df_viirs_geo     = geo_viirs(df_viirs, crs)
+    return {'df_viirs': df_viirs_geo,
+            'data_report': df_viirs_report}
 
-    return viirs_data
+    return df_viirs_raw
     # print(f"Found {len(viirs_to_load)} files") 
     # for i in viirs_to_load:
     #     print(i)
