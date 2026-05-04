251908b40772404427dc052c9319d88fda36fb07
enmanuelmorego <143163660+enmanuelmorego@users.noreply.github.com>
2026-05-04 10:22:48 +0100

    refactor of file_utils get_gilepaths function

--- src/utils/file_utils.py
+++ src/utils/file_utils.py
@@ -4,15 +4,16 @@ Module to manage and interact with files and directory
 from pathlib import Path
 import os
 
-def get_filepaths(dir_name: str) -> list[Path]: 
-    """  Function to get all the files in a directory inside the data folder
+def get_filepaths(dir_name: str, file_extension: str) -> list[Path]: 
+    """  Function to get all the files in a directory inside the data folder based on the specified file extension
 
     Args:
         dir_name (str): Name of the directory inside of data folder to get all the files names from 
+        file_extension (str): File extension to look for in the directory. Pass file extension without '.'
 
     Returns:
         list[Path]: List containing all the files inside the given folder
     """  
     dir_path = Path(os.environ['DATA_DIR'])/dir_name
-    files = list(dir_path.iterdir())
+    files = list(dir_path.glob(f"*.{file_extension}"))
     return files
