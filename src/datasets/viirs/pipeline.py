32a43b7e57ebfb25e2ef489bd9ab5e6392688202
enmanuelmorego <143163660+enmanuelmorego@users.noreply.github.com>
2026-05-04 11:39:38 +0100

    finalised VIIRS pipeline refactored

--- src/datasets/viirs/pipeline.py
+++ src/datasets/viirs/pipeline.py
@@ -3,33 +3,39 @@ import src.datasets.viirs.loader as l
 import src.datasets.viirs.transforms as t
 
 import pandas as pd
+import geopandas as gpd
+from typing import TypedDict
 
-# Temp
-import os
-from pathlib import Path
-os.environ.setdefault("DATA_DIR", str(Path(__file__).resolve().parents[3] / "data"))
-# temp
 
-def load_viirs_main(years_to_load: list[int]):
-    viirs_files     = fu.get_filepaths('VIIRS', "csv")
-    viirs_to_load   = l.select_viirs_files(viirs_files, years_to_load)
-    viirs_data      = l.load_viirs(viirs_to_load)
-    df_viirs_raw    = t.merge_viirs(viirs_data)
-    df_viirs_report = df_viirs_raw['data_report']
-    df_viirs_temp   = df_viirs_raw['df']
-    df_viirs         = t.filter_viirs(df_viirs_temp)
-    df_viirs         = df_viirs.rename(columns = {'acq_date': 'date'})
-    df_viirs["date"] = pd.to_datetime(df_viirs["date"])
-    df_viirs_geo     = geo_viirs(df_viirs, crs)
-    return {'df_viirs': df_viirs_geo,
-            'data_report': df_viirs_report}
+# Pre define complex dictionaries type hints
+class ViirsPipelineOutput(TypedDict):
+    df_viirs: gpd.GeoDataFrame
+    data_report: dict | None
 
-    return df_viirs_raw
-    # print(f"Found {len(viirs_to_load)} files") 
-    # for i in viirs_to_load:
-    #     print(i)
+def load_viirs_main(years_to_load: list[int], crs: str, dir_name: str = "VIIRS", file_extension:str = "csv") -> ViirsPipelineOutput:
+    viirs_files       = fu.get_filepaths(dir_name, file_extension)
+    viirs_to_load     = l.select_viirs_files(viirs_files, years_to_load)
+    viirs_data        = l.load_viirs(viirs_to_load)
+    df_viirs_raw      = t.merge_viirs(viirs_data)
+    dict_viirs_report = df_viirs_raw['data_report']
+    df_viirs_temp     = df_viirs_raw['df']
+    df_viirs          = t.filter_viirs(df_viirs_temp)
+    df_viirs          = df_viirs.rename(columns = {'acq_date': 'date'})
+    df_viirs["date"]  = pd.to_datetime(df_viirs["date"])
+    df_viirs_geo      = t.transform_to_geo_viirs(df_viirs, crs)
+    return {'df_viirs'   : df_viirs_geo,
+            'data_report': dict_viirs_report}
 
 if __name__ == "__main__":
     # python3 -m src.datasets.viirs.pipeline
-    years = []
-    print(load_viirs_main(years).keys())
+    # Temp
+    import os
+    from pathlib import Path
+    os.environ.setdefault("DATA_DIR", str(Path(__file__).resolve().parents[3] / "data"))
+    # temp
+    years = [2019]
+    crs = "EPSG: 4326" 
+    output = load_viirs_main(years, crs)
+    print(output.get('data_report'))
+    df_v = output.get('df_viirs')
+    print(df_v.shape)
