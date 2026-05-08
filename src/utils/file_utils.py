6bc3b15da143954accdd3d91f070086a2d6eee07
enmanuelmorego <143163660+enmanuelmorego@users.noreply.github.com>
2026-05-08 20:12:31 +0100

    Created function to open scripts to help updating parameters easily

--- src/utils/file_utils.py
+++ src/utils/file_utils.py
@@ -2,6 +2,9 @@
 Module to manage and interact with files and directory
 """
 from pathlib import Path
+import os
+import platform
+import subprocess
 
 def get_filepaths(data_dir: Path, dir_name: str, file_extension: str) -> list[Path]: 
     """  Function to get all the files in a directory inside the data folder based on the specified file extension
@@ -16,3 +19,21 @@ def get_filepaths(data_dir: Path, dir_name: str, file_extension: str) -> list[Pa
     dir_path = data_dir/dir_name
     files = list(dir_path.glob(f"*.{file_extension}"))
     return files
+
+def open_file(filename):
+    """Function that opens the file specified
+
+    Args:
+        filename (_type_): Full path of the file to open
+    """    
+    current_system = platform.system()
+    if current_system == "Windows":
+        os.startfile(filename) #type: ignore
+    elif current_system == "Darwin":
+        subprocess.call(["open","-a", "Visual Studio Code",  str(filename)])
+
+if __name__ == "__main__":
+
+    param_file = Path(__file__).parents[1]/"scripts"/"set_parameters.py"
+    print(param_file)
+    open_file(param_file)
