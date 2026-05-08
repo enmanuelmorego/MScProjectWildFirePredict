14ddb2405ec4bd3b5c6cf9941e9006faba4773b4
enmanuelmorego <143163660+enmanuelmorego@users.noreply.github.com>
2026-05-08 20:13:15 +0100

    ADded function to open file to validate_params_update function

--- src/scripts/validation_checks.py
+++ src/scripts/validation_checks.py
@@ -2,12 +2,15 @@
 Module containing functions to manage and control the run process
 """
 from datetime import date
+from pathlib import Path
+import utils.file_utils as u
 
-def validate_params_update(validation_date: date):
+def validate_params_update(validation_date: date, params_file: str = "set_parameters.py"):
     """Function to validate and check user have updated parameters before running pipeline
 
     Args:
         validation_date (date): Validation date object from set_parameters.py file
+        params_file (str): Name of the file containing the parameter values
 
     Raises:
         ValueError: Stop function if the date does not match today
@@ -15,5 +18,7 @@ def validate_params_update(validation_date: date):
     # Get today's date
     today = date.today()
     if today != validation_date:
-        raise ValueError("\n\t❌ Please update object VALIDATION_DATE with today's dates in location:\n\t   src/scripts/set_parameters.py")
- 
+        param_file_full = (Path(__file__).parent/params_file)
+        u.open_file(param_file_full)
+        raise ValueError(f"\n\t❌ Please update object VALIDATION_DATE with today's dates in location:\n\t   src/scripts/{params_file}")
+    
