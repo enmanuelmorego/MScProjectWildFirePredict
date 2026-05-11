c94a57bda9e9fdefc0269163ef8e1a00436a9d81
enmanuelmorego <143163660+enmanuelmorego@users.noreply.github.com>
2026-05-11 08:08:21 +0100

    Moved funcitont o clean grid_1 to new module

--- src/transforms/preprocessing_transforms.py
+++ src/transforms/preprocessing_transforms.py
@@ -20,7 +20,7 @@ def aggregate_viirs_to_grid(df_viirs: gpd.GeoDataFrame, df_uk_grid: gpd.GeoDataF
         pd.DataFrame: Dataframe with VIIRS data collapsed per grid, with max and mean values for each grid
             The dataframe contains only those `grid_id` where a VIIRS signal was present
     """
-    df_viirs = df_viirs.rename(columns = {'acq_date': 'date'})
+    df_viirs        = df_viirs.rename(columns = {'acq_date': 'date'})
     df_viirs_joined = gpd.sjoin(df_viirs, df_uk_grid, how = 'inner', predicate = 'within')
     df_viirs_summary = (df_viirs_joined
                         .groupby(['grid_id', 'date'], as_index = False)
@@ -69,3 +69,38 @@ def build_tabular_dataset(df_viirs: pd.DataFrame, dict_dfs: dict) -> gpd.GeoData
                                             how = 'left')
     
     return df_fwi_viirs_grid
+
+def remove_na_fwi_grid1(df: gpd.GeoDataFrame) -> gpd.GeoDataFrame:
+    """Removes grid_id = 1 from the dataset as the FWI value for grid_id 1 was missing across the sample
+    
+    During spatial validation of the FWI raster-to-grid mapping, grid_id = 1 (a coastal grid in south-west England) was found to
+    have missing FWI values for all day
+
+    Rationale
+
+        - The grid represents <0.1% of all spatial units,
+        - FWI is entirely missing for that grid across all dates,
+        - Imputation would introduce artificial meteorological signal,
+
+    the grid is removed from the modelling dataset to preserve
+    data integrity.
+
+    This is only removed IF grid_id == 1 AND fwi value is NA
+
+    Args:
+        df (gpd.GeoDataFrame): Preprocessed dataframe
+
+    Returns:
+        gpd.GeoDataFrame: Dataset excluding grid_id = 1.
+    """
+    remove = ((df['grid_id'] == 1) &
+              (df['fwi_max'].isna()))
+  
+    rr = remove.sum()
+    if rr > 0:
+        print(f"\tℹ️  Removed {rr} rows with grid_id == 1")
+    else:
+        print(f"\t✅  No grids to remove - grid_id contains data")
+
+    df_out = df[~remove].copy()
+    return df_out
