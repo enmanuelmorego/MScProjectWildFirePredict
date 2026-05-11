718e0c55a406936c48605688a7c444ad9d54b005
enmanuelmorego <143163660+enmanuelmorego@users.noreply.github.com>
2026-05-11 09:57:55 +0100

    formatted script as per convention

--- src/pipelines/fwi_pipeline.py
+++ src/pipelines/fwi_pipeline.py
@@ -7,26 +7,31 @@ from pathlib import Path
 import geopandas as gpd
 
 def load_fwi_main(df_uk_grid_in: gpd.GeoDataFrame, data_dir: Path, requested_years: list[int], grb_name: str, crs: str, dir_name: str = "FWI"):
-
+    # ------------------------
+    # FETCH REQUIREMENTS
+    # ------------------------
     fwi_path  = Path(data_dir)/dir_name
     fwi_files = l.fwi_file_availability_wrapper(data_dir, requested_years, dir_name)
-
-    # 1. Check if any files are required from CEMS API
+    # ------------------------
+    # REQUIRES FROM CEMS API
+    # ------------------------
     fetch_from_api = fwi_files['required_years']
     if fetch_from_api:
         l.fwi_fetch_from_api(fetch_from_api, fwi_path)
         # Refresh requirements to include newly downloaded data
         fwi_files = l.fwi_file_availability_wrapper(data_dir, requested_years, dir_name)
-
-    # 2. If Grib file needs to be transformed to csv
+    # ------------------------
+    # REQUIRES TRANSFORM GRIB TO CSV
+    # ------------------------
     grib_to_csv = fwi_files['available_grib']
     if grib_to_csv:
         for g in grib_to_csv:
             t.transform_grib_to_csv(fwi_path = fwi_path, grib_fname = g, grb_name = grb_name, df_uk_grid = df_uk_grid_in, crs_val = crs)
         # Refresh requirements to include newly transformed data
         fwi_files = l.fwi_file_availability_wrapper(data_dir, requested_years, dir_name)
-
-    # 3. Load csv data
+    # ------------------------
+    # LOAD CSV
+    # ------------------------
     fwi_csv_files = fwi_files['available_csv']
     df_fwi        = l.fwi_load_csv_files(fwi_csv_files, fwi_path)
 
