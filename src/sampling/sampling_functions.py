22ad05565aa1a0824cccae56d46323d9d8341fa5
enmanuelmorego <143163660+enmanuelmorego@users.noreply.github.com>
2026-05-30 19:13:46 +0100

    REmoved the multiple responsibilities from function and now only returns a string og the sampled composite_key

--- src/sampling/sampling_functions.py
+++ src/sampling/sampling_functions.py
@@ -1,29 +1,27 @@
 import pandas as pd
 import random as rd
 
-def extract_temporal_samples(df_fire_in: pd.DataFrame, 
-                             current_grid_id_in: str,
-                             current_date_in: pd.Timestamp, 
-                             dict_samples_in: dict, 
+
+def extract_temporal_samples(df_fire_in         : pd.DataFrame, 
+                             current_grid_id_in : str,
+                             current_date_in    : pd.Timestamp, 
+                             used_comp_keys_in  : set[str], 
                              temporal_gap_months: int = 6,
-                             date_span_days: int      = 15,
-                             span_limit_days: int     = 60):
+                             date_span_days     : int = 15,
+                             span_limit_days    : int = 60) -> str | None:
     
-    sample_found      = False
     working_span_days = date_span_days
-    unavailable_comp_keys = dict_samples_in['used_comp_keys']
-    composite_key_idx     = df_fire_in.columns.get_loc('composite_key')
     # Calculate date date to extract samples from
     sample_date_centroid = current_date_in - pd.DateOffset(months = temporal_gap_months)
 
     # Find the samples
-    while not sample_found:
+    while True:
         # Generate date range
         min_date = sample_date_centroid - pd.DateOffset(days = working_span_days)  
         max_date = sample_date_centroid + pd.DateOffset(days = working_span_days) 
         # Extract potential set of samples 
         df_potential_samples = df_fire_in[  (df_fire_in['grid_id'] == current_grid_id_in) 
-                                          & (df_fire_in['composite_key'] not in unavailable_comp_keys)
+                                          & (~df_fire_in['composite_key'].isin(used_comp_keys_in))
                                           & (df_fire_in['date'] >= min_date)
                                           & (df_fire_in['date'] <= max_date)]
         # Get total number of potential samples
@@ -34,16 +32,12 @@ def extract_temporal_samples(df_fire_in: pd.DataFrame,
             working_span_days += date_span_days
             # If span limit is exceed, then no nample is found
             if working_span_days > span_limit_days:
-                dict_samples_in['temporal_sample_comp_key'].append(None)
-                sample_found = True
+                return None
+            continue
             
         # Randomly select index of row to select
-        select_row = rd.randint(0, int_potential_samples)
-        temporal_sample = df_potential_samples.iloc[select_row, composite_key_idx]  # type: ignore
-        dict_samples_in['temporal_sample_comp_key'].append(temporal_sample)
-        dict_samples_in['used_comp_keys'].add(temporal_sample)
-        sample_found = True
-    return dict_samples_in
+        temporal_sample = str(df_potential_samples.sample(n=1)['composite_key'].iloc[0])  # type: ignore
+        return temporal_sample
 
 if __name__ == '__main__':
     import pandas as pd
@@ -67,9 +61,9 @@ if __name__ == '__main__':
     samples_dict = {'fire_lbl_comp_key'   : [],
                 'temporal_sample_comp_key': [],
                 'spatial_sample_comp_key' : [],
-                'used_comp_keys'          : set()}
+                'used_comp_keys'          : {'A_20200615'}} #set()}
     
     print(extract_temporal_samples(df_fire_in=df_fire,
                              current_grid_id_in="A",
                              current_date_in=pd.Timestamp("2020-12-15"),
-                             dict_samples_in=samples_dict))
+                             used_comp_keys_in = samples_dict['used_comp_keys']))
