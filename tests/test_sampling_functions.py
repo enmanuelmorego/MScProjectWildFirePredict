758c05af87e1625a17ef4eabb781ec26fb2cd6fd
enmanuelmorego <143163660+enmanuelmorego@users.noreply.github.com>
2026-06-01 22:17:17 +0100

    Refactored tests to account for fire_lbl logic

--- tests/test_sampling_functions.py
+++ tests/test_sampling_functions.py
@@ -20,7 +20,8 @@ def test_valid_sample_extract_temporal_sample():
                                           "1_20201215",
                                           "2_20200615",
                                           "2_20201215",
-                                          "3_20201215"]})
+                                          "3_20201215"],
+                        "fire_lbl": [False,False,False,False,False,False,False]})
     
     samples_dict = {'fire_lbl_comp_key'       : [],
                     'temporal_sample_comp_key': [],
@@ -34,9 +35,10 @@ def test_valid_sample_extract_temporal_sample():
     assert sample in {"1_20200615", "1_20200620"}
 
 def test_exclude_if_used_extract_temporal_sample():
-    df_fire = pd.DataFrame({"grid_id": [1, 1, 1, 1, 2, 2, 3],
+    df_fire = pd.DataFrame({"grid_id": [1, 1, 1, 1, 1, 2, 2, 3],
                             "date": pd.to_datetime(["2020-06-15",  
                                                     "2020-06-20",  
+                                                    "2020-06-10",
                                                     "2020-07-01",  
                                                     "2020-12-15",  
                                                     "2020-06-15",
@@ -44,16 +46,25 @@ def test_exclude_if_used_extract_temporal_sample():
                                                     "2020-12-15"]),
                             "composite_key": ["1_20200615",
                                               "1_20200620",
+                                              "1_20200610",
                                               "1_20200701",
                                               "1_20201215",
                                               "2_20200615",
                                               "2_20201215",
-                                              "3_20201215"]})
+                                              "3_20201215"],
+                            'fire_lbl': [False,
+                                         False,
+                                         True,
+                                         False,
+                                         False,
+                                         False,
+                                         False,
+                                         False]})
                
     samples_dict = {'fire_lbl_comp_key'       : [],
                     'temporal_sample_comp_key': [],
                     'spatial_sample_comp_key' : [],
-                    'used_comp_keys'          : {"A_20200620"}}
+                    'used_comp_keys'          : {"1_20200620"}}
              
     sample = sf.extract_temporal_sample(df_all_in=df_fire,
                                     current_grid_id_in=1,
@@ -76,7 +87,8 @@ def test_no_sample_found_extract_temporal_sample():
                                               "1_20201215",
                                               "2_20200615",
                                               "2_20201215",
-                                              "3_20201215"]})
+                                              "3_20201215"],
+                            "fire_lbl": [False,False,False,False,False,False,False]})
                
     samples_dict = {'fire_lbl_comp_key'       : [],
                     'temporal_sample_comp_key': [],
@@ -104,7 +116,8 @@ def test_window_expand_extract_temporal_sample():
                                               "1_20201215",
                                               "2_20200615",
                                               "2_20191120",
-                                              "3_20201215"]})
+                                              "3_20201215"],
+                            "fire_lbl": [False,False,False,False,False,False,False]})
                
     samples_dict = {'fire_lbl_comp_key'       : [],
                     'temporal_sample_comp_key': [],
@@ -123,6 +136,34 @@ def test_window_expand_extract_temporal_sample():
     assert sample_short == None
     assert sample_long  == "2_20191120"
 
+def test_non_valid_fire_candidate_sample_extract_temporal_sample():
+    df_fire = pd.DataFrame({"grid_id": [1, 1, 1, 1, 2, 2, 3],
+                            "date": pd.to_datetime(["2020-06-15",  
+                                                    "2020-06-20",  
+                                                    "2020-07-01",  
+                                                    "2020-12-15",  
+                                                    "2020-06-15",
+                                                    "2020-12-15",
+                                                    "2020-12-15"]),
+                            "composite_key": ["1_20200615",
+                                              "1_20200620",
+                                              "1_20200701",
+                                              "1_20201215",
+                                              "2_20200615",
+                                              "2_20201215",
+                                              "3_20201215"],
+                            "fire_lbl": [True,True,True,False,False,False,False]})
+    
+    samples_dict = {'fire_lbl_comp_key'       : [],
+                    'temporal_sample_comp_key': [],
+                    'spatial_sample_comp_key' : [],
+                    'used_comp_keys'          : set()}
+    
+    sample = sf.extract_temporal_sample(df_all_in=df_fire,
+                                       current_grid_id_in=1,
+                                       current_date_in=pd.Timestamp("2020-12-15"),
+                                       used_comp_keys_in = samples_dict['used_comp_keys'])
+    assert sample == None
 # -----------------------------------
 # TEST extract_temporal_sample()
 # -----------------------------------
