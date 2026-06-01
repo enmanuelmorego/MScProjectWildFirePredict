7b8128b9212b60506cc3a610843bb9cba0e52055
enmanuelmorego <143163660+enmanuelmorego@users.noreply.github.com>
2026-06-01 06:36:23 +0100

    Wrapped sampling procedure into pipeline function

---
+++ src/pipelines/sampling_pipeline.py
@@ -0,0 +1,47 @@
+import pandas as pd
+import geopandas as gdp
+import src.sampling.sampling_functions as sf
+from typing import cast 
+
+def create_samples_dict(df_in: gdp.GeoDataFrame):
+
+    # Create subsets of dataframes
+    df      = df_in.copy()
+    df_fire = df_in.copy()
+    df_fire = df_fire[df_fire['fire_lbl'] == True]
+
+    # Extract dataset that has never seen a fire 
+    df_nofire = df_in.copy()
+    df_nofire = df_nofire[~df_nofire['grid_id'].isin(df_fire['grid_id'])]
+
+    # Initialise dictionary to store samples
+    samples_dict = {'fire_lbl_comp_key'       : [],
+                    'temporal_sample_comp_key': [],
+                    'spatial_sample_comp_key' : [],
+                    'spatial_sample_dist'     : [],
+                    'used_comp_keys'          : set()}
+    
+    # Extract samples
+    for row in df_fire.itertuples():
+        # Extract relevant elements from fire observation
+        composite_tgt = row.composite_key
+        grid_tgt      = cast(int, row.grid_id)
+        date_tgt      = cast(pd.Timestamp, row.date)
+        x_tgt         = cast(float, row.x_coord)
+        y_tgt         = cast(float, row.y_coord)
+        used_comp_key = samples_dict['used_comp_keys']
+        # Prepare datasets to sample from (reduce size for more efficiency)
+        df_to_sample_temporal = df[df['grid_id'] == grid_tgt]
+        df_to_sample_spatial  = df_nofire[df_nofire['date'] == date_tgt]
+        # Find samples
+        temp_sample                 = sf.extract_temporal_sample(df_to_sample_temporal, grid_tgt, date_tgt, used_comp_key)
+        spatial_sample, sample_dist = sf.extract_spatial_sample(df_to_sample_spatial, x_tgt, y_tgt)
+        # SAve results in dictionary 
+        samples_dict['fire_lbl_comp_key'].append(composite_tgt)
+        samples_dict['temporal_sample_comp_key'].append(temp_sample)
+        samples_dict['spatial_sample_comp_key'].append(spatial_sample)
+        samples_dict['spatial_sample_dist'].append(sample_dist)
+        samples_dict['used_comp_keys'].add(temp_sample)
+        samples_dict['used_comp_keys'].add(spatial_sample)
+    
+    return samples_dict
