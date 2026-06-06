0203162766de779660812385c2db444a7f6e9784
enmanuelmorego <143163660+enmanuelmorego@users.noreply.github.com>
2026-06-06 12:58:56 +0100

    fetch relevant parameters from set_parameters

--- src/scripts/run_sentinel2_fetch.py
+++ src/scripts/run_sentinel2_fetch.py
@@ -1,3 +1,27 @@
 """
 Module that fetches the Sentinel2 data from GEE
 """
+from scripts.set_parameters import VALIDATION_DATE, PARAMETERS
+
+import scripts.validation_checks as vc
+import pipelines.tabular_load_pipeline as lp
+import transforms.preprocessing_transforms as pp
+import pipelines.sampling_pipeline as sp
+import sampling.sampling_functions as sf
+
+
+
+def run_tabular():
+    # ------------------------
+    # VALIDATE RUN PARAMETERS
+    # ------------------------
+    vc.validate_params_update(VALIDATION_DATE)
+    # ------------------------
+    # EXTRACT PARAMETERS 
+    # ------------------------
+    YEAR_FILTER = PARAMETERS['YEAR_FILTER']
+    DATA_DIR    = PARAMETERS['DATA_DIR']
+    # ------------------------
+    # LOAD SAMPLED DATA
+    # ------------------------
+    
