84115669d4f1d14f2eb6ac3e6d65cb80eabf1090
enmanuelmorego <143163660+enmanuelmorego@users.noreply.github.com>
2026-06-07 10:14:45 +0100

    ADded function to save sentinel data. refactored from existing

--- src/data_io/sentinel2_io.py
+++ src/data_io/sentinel2_io.py
@@ -1,3 +1,5 @@
+from pathlib import Path
+import os
 import ee
 import numpy as np
 import requests
@@ -51,3 +53,29 @@ def fetch_sentinel_data(geom: ee.Geometry, date_str: str, satelite_params: dict 
     with io.BytesIO(response.content) as f:
         raw_data = tifffile.imread(f)
         return raw_data
+    
+
+def save_sentinel_nps(image_list: list, label_list: list, composite_key_list: list, batch_name: str, data_dir: Path) -> None:
+    """Saves Sentinel2 data to disk as compressed `.npz` file
+
+    The functions converts the downloaded image, label and composite key into numpy arrays and stores them in a single compressed file
+    The resulting file contains three arrays:
+
+    - `x`: Sentinel-2 image data
+    - `y`: Labels associated with each image
+    - `composite_key`: Composite keys corresponding to each observation
+
+    Args:
+        image_list (list): List containing the Sentinel-2 image arrays
+        label_list (list): List containing the target labels associated with each image
+        composite_key_list (list): List containing the composite keys corresponding to each observation
+        batch_name (str): Name of the current batch. This value is used to generate the output filename
+        data_dir (Path): Root data directory where the `Sentinel2` subdirectory is located
+    """
+    x     = np.array(image_list)
+    y     = np.array(label_list)
+    ids   = np.array(composite_key_list)
+    fname = f"{batch_name}.npz"
+    fout  = data_dir/'Sentinel2'/fname
+    np.savez_compressed(fout, x=x, y=y, composite_key=ids)
+    print(f"\n\t 🎉 Success! Saved {fname} ({x.nbytes / 1e6:.2f}")
