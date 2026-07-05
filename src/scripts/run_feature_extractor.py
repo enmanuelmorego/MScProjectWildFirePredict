210f835de30d03a5c3975f5174ce7973c388f5b6
enmanuelmorego <143163660+enmanuelmorego@users.noreply.github.com>
2026-07-05 20:40:08 +0100

    TEsting the new functins for feature extraction

--- src/scripts/run_feature_extractor.py
+++ src/scripts/run_feature_extractor.py
@@ -2,7 +2,7 @@
 Module that runs and performs the feature extraction of Sentinel2 data using ResNet18
 """
 from scripts.set_parameters import PARAMETERS, VALIDATION_DATE
-from transforms.resnet_feature_extractor import SentinelData, ResNetFeatExtractor
+from transforms.resnet_feature_extractor import ResNetFeatExtractor, extract_resnet_features
 
 import utils.validation_checks as vc
 import utils.file_utils as fu
@@ -20,8 +20,21 @@ def run_feature_extractor():
     # LOAD SENTINEL DATA
     # -------------------------------
     files_sentinel = fu.get_filepaths(DATA_DIR, "Sentinel2", "npz")
-    return(files_sentinel)
+    # -------------------------------
+    # RESNET-18 MODEL
+    # -------------------------------
+    # Initialise model
+    resnet_model = ResNetFeatExtractor()
+    # Disable training behaviour since the model is used only for feature extraction
+    resnet_model.eval()
+    # Perform feature extraction
+    df_features = extract_resnet_features(files_sentinel[0:2], resnet_model)
+
+    vc.validate_resnet_feature_extractor(df_features)
+
+    return(df_features)
 
 if __name__ == "__main__":
     test = run_feature_extractor()
-    print(test)
+    print(test.head())
+
