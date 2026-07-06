8b4bff5d1b3e04c1a5d5b949a24f2faed8e55a1d
enmanuelmorego <143163660+enmanuelmorego@users.noreply.github.com>
2026-07-06 06:29:30 +0100

    refactored passed all tests so far

--- tests/test_validation_checks.py
+++ tests/test_validation_checks.py
@@ -1,5 +1,6 @@
 import utils.validation_checks as vc
 import pandas as pd 
+import pytest
 
 def test_validate_resnet_feature_extractor_no_duplicates():
     df_test = pd.DataFrame({'composite_key': ['001', '002','003'],
@@ -9,6 +10,13 @@ def test_validate_resnet_feature_extractor_no_duplicates():
 
 def test_validate_resnet_feature_extractor_real_duplicates():
     df_test = pd.DataFrame({'composite_key': ['001','001','001', '002','003'],
-                            'feat_01': [0,11,1,3 ,2],
-                            'feat_02': [0,11,1,8,110]})
+                            'feat_01': [0,0,0,3 ,2],
+                            'feat_02': [0,0,0,8,110]})
     assert vc.validate_resnet_feature_extractor(df_test) is None
+
+def test_validate_resnet_feature_extractor_different_duplicates():
+    df_test = pd.DataFrame({'composite_key': ['001','001','001', '002','003'],
+                            'feat_01': [0,11,13,3 ,2],
+                            'feat_02': [0,11,13,8,110]})
+    with pytest.raises(ValueError):
+        vc.validate_resnet_feature_extractor(df_test)
