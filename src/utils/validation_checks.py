af0b75135c4a4c8adbb9d2a26d698a66c425e4c9
enmanuelmorego <143163660+enmanuelmorego@users.noreply.github.com>
2026-07-06 06:52:38 +0100

    Added docstring to composite key valdiatio checls

--- src/utils/validation_checks.py
+++ src/utils/validation_checks.py
@@ -39,28 +39,46 @@ def validate_data_load_dict(dict_dfs: dict[int, pd.DataFrame | None]) -> None:
                                 "\nPlease add the missing year(s) value(s) to [YEAR_FILTER] list in [src/scripts/set_parameters.py]"
                                 "\nand run script [src/scripts/run_tabular.py] to create the missing file(s)")
     
-def validate_resnet_feature_extractor(df_features: pd.DataFrame):
+def validate_resnet_feature_extractor(df_features: pd.DataFrame) -> None:
+    """Checks whether there are duplicated composite keys in the dataframe from the feature extraction process
+    If duplicated composite keys do exists, ensure that all features are the same (real duplicates)
 
+    Args:
+        df_features (pd.DataFrame): Data frame from feature extraction process
+
+    Raises:
+        ValueError: When the same composite key has multiple feature values within the same composite key
+    """
     # Find duplicated composite keys
-    df_duplicated_keys = df_features.loc[df_features.duplicated(subset='composite_key'),
-                                         'composite_key']
-    int_duplicated_keys = df_duplicated_keys.shape[0]
+    df_duplicated_keys  = df_features.groupby("composite_key").filter(lambda x: len(x) > 1)
+    int_duplicated_rows = df_duplicated_keys.shape[0]
+    duplicated_keys     = df_duplicated_keys['composite_key'].unique()
+    int_duplicated_keys = duplicated_keys.shape[0]
+
+    if df_duplicated_keys.empty:
+        return
+    
     if int_duplicated_keys > 0:
-        print(f"⚠️  Total [{int_duplicated_keys}] duplicated composite keys...")
+        print(f"⚠️  Total [{int_duplicated_keys}] duplicated composite keys ({int_duplicated_rows} rows)")
+
     # Check that all of the duplicated composite_keys contain the same features (real duplicated)
-    df_to_check = df_features[df_features['composite_key'].isin(df_duplicated_keys)]
-    # Check for real duplicates
-    df_checked  = df_to_check[df_to_check.duplicated()]
-    print("=== Checked ===")
-    print(df_checked)
-    if df_checked.shape[0] != df_duplicated_keys.shape[0]:
-        print(f"❌  ERROR  [{df_duplicated_keys.shape[0] - df_checked.shape[0]}] duplicated composite keys WITH DIFFERENT FEATURE VALUES...")
+    df_to_check = df_features[df_features['composite_key'].isin(duplicated_keys)]
+
+    # Check for real duplicates - Expect that all duplicated composite keys also appear as duplicated when considering all the columns
+    # If there are less rows in df_checked vs duplicated composite_keys means that the same composite key has different feature values in the rows 
+    df_checked       = df_to_check[df_to_check.duplicated(keep = False)]
+    int_cheched_rows = df_checked.shape[0] 
+
+    if int_cheched_rows != int_duplicated_rows:
+        non_real_duplicated = set(df_duplicated_keys['composite_key']) - set(df_checked['composite_key'])
+        raise ValueError(f"\n❌  ERROR  \nFound [{df_duplicated_keys.shape[0] - df_checked.shape[0]}] duplicated composite keys WITH DIFFERENT FEATURE VALUES...\n"
+                         f"Inspect values {non_real_duplicated}")
 
 
 
 
 if __name__ == "__main__":
-    df_test = pd.DataFrame({'composite_key': ['001','001', '002','003'],
-                            'feat_01': [0,1,2,3],
-                            'feat_02': [0,110,10,1]})
+    df_test = pd.DataFrame({'composite_key': ['001','001','001', '002','003'],
+                            'feat_01': [0,11,1,3 ,2],
+                            'feat_02': [0,11,1,8,110]})
     validate_resnet_feature_extractor(df_test)
