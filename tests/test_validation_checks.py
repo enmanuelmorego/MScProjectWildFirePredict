6ebd4538ab71c90649ef68c67e326ca5dc7ad9ff
enmanuelmorego <143163660+enmanuelmorego@users.noreply.github.com>
2026-07-06 06:53:11 +0100

    test for composite key duplicated

--- tests/test_validation_checks.py
+++ tests/test_validation_checks.py
@@ -20,3 +20,26 @@ def test_validate_resnet_feature_extractor_different_duplicates():
                             'feat_02': [0,11,13,8,110]})
     with pytest.raises(ValueError):
         vc.validate_resnet_feature_extractor(df_test)
+
+def test_validate_resnet_feature_extractor_one_duplicate_differs():
+    df_test = pd.DataFrame({"composite_key": ["001", "001", "001"],
+                             "feat_01": [5, 5, 99],
+                             "feat_02": [8, 8, 8]})
+
+    with pytest.raises(ValueError):
+        vc.validate_resnet_feature_extractor(df_test)
+
+def test_validate_resnet_feature_extractor_multiple_duplicate_groups():
+    df_test = pd.DataFrame({"composite_key": ["001", "001", "002", "002"],
+                            "feat_01": [1, 1, 5, 6],
+                            "feat_02": [2, 2, 8, 8]})
+
+    with pytest.raises(ValueError):
+        vc.validate_resnet_feature_extractor(df_test)
+
+def test_validate_resnet_feature_extractor_unsorted_duplicates():
+    df_test = pd.DataFrame({"composite_key": ["002", "001", "002", "001"],
+                            "feat_01": [3, 1, 3, 1],
+                            "feat_02": [4, 2, 4, 2]})
+
+    assert vc.validate_resnet_feature_extractor(df_test) is None
