f675763867164ea2cb671682a15bcc721d377b99
enmanuelmorego <143163660+enmanuelmorego@users.noreply.github.com>
2026-07-10 07:41:29 +0100

    ADded helper function to find the latest sentinel2 missing log file to load

--- src/utils/datasets_utils.py
+++ src/utils/datasets_utils.py
@@ -3,6 +3,10 @@ Module that contains helper files and functions to transform datasets
 """
 import pandas as pd
 import geopandas as gpd
+import re
+
+from pathlib import Path 
+from datetime import datetime 
 
 def extract_year_range(df: pd.DataFrame) -> pd.DataFrame:
     """  Generate a daily date range covering full calendar years based on the
@@ -38,6 +42,54 @@ def extract_year_range(df: pd.DataFrame) -> pd.DataFrame:
     dates_df = pd.DataFrame({'date': dates_covered})
     dates_df['join_key'] = 1
     return dates_df
+
+def get_latest_missing_sentinel_log_filepath(files_paths: list[Path], file_name_pattern: str = r"missing_sentinel2_images") -> Path:
+    """Takes a list of file paths, containing multiple logs from Sentinel-2 data download process
+    It iterates thru the list of file paths and finds only the files that matches the value in parameter `file_name_pattern` 
+    Extracts the date from the file name and finds the latest / most recent file name based on the date used in the file name
+
+    Args:
+        files_paths (list[Path]): List of file paths - Assumes file names contains a time stamp in format `YYYY-mm-dd`
+        file_name_pattern (str, optional): Pattern of the files the funciton is interested in. Defaults to r"missing_sentinel2_images".
+
+    Raises:
+        ValueError: When no date value can be extracted from file name. This means the file name does not contain the pattern "%Y-%m-%d"
+        FileNotFoundError: Raises when, if after finishing the iteration and processes no file path is found 
+
+    Returns:
+        Path: File path of the latest file to load
+    """
+    # Initialise objects
+    latest_file_date = None
+    file_to_load     = None
+
+    # Find latest files
+    for f in files_paths:
+        # Skip if the file does not match the expected pattern 
+        if re.search(file_name_pattern, str(f)) is None:
+            continue
+    
+        # Extract date
+        file_date = re.search(r"\d{4}-\d{2}-\d{2}", str(f))
+    
+        # Raise value error if no date value could be parsed from filename
+        if file_date is None:
+            raise ValueError(f"\n❌  ERROR  \nCould not extract date value from filename {f}")
+    
+        # Extract date from file to find latest file 
+        file_date = datetime.strptime(file_date.group(), "%Y-%m-%d")
+        # Update latest file object to match latest file
+        if latest_file_date is None or file_date > latest_file_date:
+            latest_file_date = file_date
+            file_to_load     = f
+
+    # IF there is no file to load, then raise value error         
+    if file_to_load is None:
+        raise FileNotFoundError(f"\n❌  ERROR  \nCould not find a Sentinel-2 missing files log file to load")
+    
+    # Return succesfully found file path
+    print("📂 Loading Failed Sentinel-2 log from: {file_date}\nFile Name: {file_to_load}")
+    return file_to_load 
 
 def combine_dict_to_geodf(dict_in: dict[int, pd.DataFrame], crs: str) -> gpd.GeoDataFrame: 
 
