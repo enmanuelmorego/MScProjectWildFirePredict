Fixed the test/train leakage in fine tuning section

src/scripts/s03.1_run_resnet18_finetune.py
@@ -42,14 +42,23 @@ df_metadata = pd.DataFrame({"idx"          : np.arange(len(all_x)),
                             "date"         : all_dates,
                             'composite_key': all_composite_keys,})
 # Split into train and test
-df_train, df_test = mu.train_test_temporal_split(df_metadata, sort_col = 'date', train_size = 0.7)
-vc.validate_train_test_split(df_train, df_test)
+df_train, df_validation, df_test = mu.train_validate_test_temporal_split(df_metadata, sort_col = 'date', train_size = 0.6, val_size=0.2, test_size=0.2)
+vc.validate_train_validation_test_split(df_train, df_validation, df_test)
+# Save train, validation, test 
+df_train_val_test = pd.DataFrame({"composite_key": pd.concat([df_train["composite_key"],
+                                                              df_validation["composite_key"],
+                                                              df_test["composite_key"]], ignore_index=True),
+                                  "split_category": (["train"] * len(df_train) +
+                                                     ["validation"] * len(df_validation) +
+                                                     ["test"] * len(df_test))})
+df_train_val_test.to_csv(DATA_DIR/"MLInputs"/"train_val_test_split.csv")
+
 # Extract indices to fetch observations from the arrays 
-train_idx = df_train["idx"].to_numpy()
-test_idx  = df_test["idx"].to_numpy()
+train_idx      = df_train["idx"].to_numpy()
+validation_idx = df_validation["idx"].to_numpy()
 # Create datasets and dataloaders
-image_dataset = {'train': FineTuneDataset(all_x[train_idx], all_y[train_idx]), # type: ignore
-                 'val'  : FineTuneDataset(all_x[test_idx],  all_y[test_idx])}  # type: ignore
+image_dataset = {'train': FineTuneDataset(all_x[train_idx],       all_y[train_idx]), # type: ignore
+                 'val'  : FineTuneDataset(all_x[validation_idx],  all_y[validation_idx])}  # type: ignore
 dataloaders   = {'train': torch.utils.data.DataLoader(image_dataset['train'], batch_size=dataloaders_batch_size, shuffle=True,  num_workers=dataloaders_num_workers),
                  'val'  : torch.utils.data.DataLoader(image_dataset['val'],   batch_size=dataloaders_batch_size, shuffle=False, num_workers=dataloaders_num_workers)}
 dataset_sizes = {x: len(image_dataset[x]) for x in ['train', 'val']}